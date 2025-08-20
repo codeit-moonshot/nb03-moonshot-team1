095366cbc84c22c728c4cc6ed0c7ed84eb98ae72
choYoungWook <wwwe2410@naver.com>
2025-08-20 14:38:51 +0900

    chore/최신 형태로 수정

--- src/modules/auth/service.ts
+++ src/modules/auth/service.ts
@@ -2,12 +2,12 @@ import ApiError from "@/errors/ApiError";
 import authRepo from "@/modules/auth/repo";
 import { RegisterDto } from "./dto/register.dto";
 
-const register = async (registerDto: RegisterDto) => {
-  const existingUser = await authRepo.findByEmail(registerDto.email);
+const register = async (data: RegisterDto) => {
+  const existingUser = await authRepo.findByEmail(data.email);
   if (existingUser) {
     throw ApiError.conflict("이미 사용 중인 이메일입니다.");
   }
-  const user = await authRepo.createAuth(registerDto);
+  const user = await authRepo.createAuth(data);
   return user;
 };
 
--- src/modules/auth/validator.ts
+++ src/modules/auth/validator.ts
@@ -1,30 +1,7 @@
 import { z } from 'zod';
 import type { RequestHandler } from 'express';
-import dns from 'dns/promises';
+import { authCreateSchema } from '@/modules/auth/dto/register.dto';
 import ApiError from '@/errors/ApiError';
-
-const emailWithMX = z.string().refine(async (email) => {
-  const domain = email.split('@')[1];
-  try{
-    const records = await dns.resolveMx(domain);
-    return records && records.length > 0;
-  } catch {
-    return false;
-  }
-}, {
-  message: '유효한 이메일 주소가 아닙니다.'
-});
-
-const userCreateSchema = z.object({
-  email: emailWithMX,
-  name: z.string()
-  .min(2, '이름은 최소 2자 이상이어야 합니다')
-  .max(20, '이름은 최대 20자 이하여야 합니다')
-  .regex(/^[a-zA-Z0-9가-힣]+$/, '이름에 특수문자는 사용할 수 없습니다.'),
-  password: z.string()
-  .min(8, '비밀번호는 최소 8자 이상이어야 합니다').max(20, '비밀번호는 최대 20자 이하여야 합니다'),
-  profileImage: z.url('이미지 URL 형식이 올바르지 않습니다.').optional()
-});
 
 const validateAuthRegister: RequestHandler = async (req, res, next) => {
   try {
@@ -34,7 +11,7 @@ const validateAuthRegister: RequestHandler = async (req, res, next) => {
       password: req.body.password,
       profileImage: req.body.profileImage
     }
-    await userCreateSchema.parseAsync(parsedBody);
+    await authCreateSchema.parseAsync(parsedBody);
     next();
   } catch (err) {
     if (err instanceof z.ZodError) {
