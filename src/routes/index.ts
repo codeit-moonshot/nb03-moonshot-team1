3459faeee91e3a690590c1e39e7c10c01544be0e
selentia <admin@duna.me>
2025-08-21 11:13:20 +0900

    chore: 라우터 예시코드 주석처리

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,13 @@
 /**
- * 라우터 작성이 완료되면, 예시와 같이 import 및 router.use를 추가하시면 됩니다.
+ * 라우터 작성이 완료되면, 주석 처리한 예시와 같이 import 및 router.use를 추가하시면 됩니다.
  */
 
 import { Router } from 'express';
 
-import exampleRoutes from '@/modules/_example/router';
+//import exampleRoutes from '../_example/router';
 
 const router = Router();
 
-router.use('/example', exampleRoutes);
+//router.use('/example', exampleRoutes);
 
 export default router;
