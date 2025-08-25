c0223c8c6f6f3d3455a21656f0b7fbef4e8d9f55
somangYu <yu18100356@gmail.com>
2025-08-25 14:10:21 +0900

    fix: 하위 할 일 라우터 상대 경로 제거

--- src/modules/subtasks/router.ts
+++ src/modules/subtasks/router.ts
@@ -1,18 +1,12 @@
 import { Router } from 'express';
 
-import * as subtaskController from './controller';
-import { validateSubtaskCreate } from './validator';
+import * as subtaskController from '#modules/subtasks/controller';
+import { validateSubtaskCreate } from '#modules/subtasks/validator';
 
 const router = Router({ mergeParams: true });
 
-router
-  .route('/')
-  .post(validateSubtaskCreate, subtaskController.createSubtask)
-  .get(subtaskController.getSubtaskList);
+router.route('/').post(validateSubtaskCreate, subtaskController.createSubtask).get(subtaskController.getSubtaskList);
 
-router
-  .route('/:subtaskId')
-  .patch(subtaskController.updateSubtask)
-  .delete(subtaskController.deleteSubtask);
+router.route('/:subtaskId').patch(subtaskController.updateSubtask).delete(subtaskController.deleteSubtask);
 
 export default router;
