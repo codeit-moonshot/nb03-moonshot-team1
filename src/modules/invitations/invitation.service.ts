72eb58aaf025017f92f3c62f135365dffdfb563a
HsunJ_Local <gkstjswo122@gmail.com>
2025-08-26 17:35:49 +0900

    chore/코드 컨벤션 수정

--- src/modules/invitations/invitation.service.ts
+++ src/modules/invitations/invitation.service.ts
@@ -7,14 +7,10 @@ const acceptInvitation = async (acceptInvitationDto: AcceptInvitationDto): Promi
   const { email, projectId, invitationId, ...rest } = acceptInvitationDto; 
   const data = { projectId, userId: 2, role: rest.role };
   const invitation = await invitationRepo.findInvitationById(invitationId);
-  if (!invitation) {
-    throw ApiError.notFound("잘못된 초대입니다.");
-  }
+  if (!invitation) throw ApiError.notFound("잘못된 초대입니다.");
   
-  console.log('멤버 생성');
   await invitationRepo.createMember(data);
   await invitationRepo.remove(invitationId);
-  console.log('삭제 완료');
 };
 
 export default {
