b9a8b16b65f99f7617cf776356aaf96870d8fbe0
HsunJ_Local <gkstjswo122@gmail.com>
2025-08-26 10:16:34 +0900

    fix/초대 발송 부분 디렉토리 변경

--- src/modules/invitations/repo.ts
+++ src/modules/invitations/repo.ts
@@ -1,5 +1,18 @@
 import prisma from '#prisma/prisma';
-import type { CreateMemberDto } from './dto/invitationDto';
+import type { InvitationDto, CreateMemberDto } from './dto/invitationDto';
+
+const create = async (data: InvitationDto):
+  Promise<{ id: number }> => {
+  return await prisma.invitation.create({
+    data: {
+      project: { connect: { id: data.projectId } },
+      inviter: { connect: { id: data.inviter } },
+      email: data.targetEmail,
+      token: data.invitationToken
+    },
+    select: { id: true }
+  });
+}
 
 export const getToken = () => {
 
@@ -22,6 +35,7 @@ export const createMember = (data: CreateMemberDto) => {
 }
 
 export default {
+  create,
   findInvitationById,
   createMember
 }
