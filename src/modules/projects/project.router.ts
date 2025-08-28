584da2705c2a7a41fc7d9aa5837bf51c88500bae
selentia <admin@duna.me>
2025-08-28 18:31:43 +0900

    feat: 라우팅 추가

--- src/modules/projects/project.router.ts
+++ src/modules/projects/project.router.ts
@@ -1,19 +1,34 @@
 import { Router } from 'express';
 import projectController from './project.controller';
 import validateProject from './project.validator';
+import { requireAuth } from '#middlewares/requireAuth';
+import projectTasksController from '#modules/tasks/projects/projectTasks.controller';
+import projectTasksValidator from '#modules/tasks/projects/projectTasks.validator';
 
 const router = Router();
 
-router
-  .route('/')
-  .post(validateProject.validateCreateProject, projectController.createProject);
+router.route('/').post(validateProject.validateCreateProject, projectController.createProject);
 
-router
-  .route('/:projectId/invitations') 
-  .post(projectController.createInvitation)
+router.route('/:projectId/invitations').post(projectController.createInvitation);
 
+router.route('/:projectId/users/:userId').delete(projectController.excludeMember);
+
+/**
+ * /projects/:projectId/tasks
+ */
 router
-  .route('/:projectId/users/:userId')
-  .delete(projectController.excludeMember)
+  .route('/:projectId/tasks')
+  .post(
+    requireAuth,
+    projectTasksValidator.validateProjectId,
+    projectTasksValidator.validateCreateTaskBody,
+    projectTasksController.createTaskInProject
+  )
+  .get(
+    requireAuth,
+    projectTasksValidator.validateProjectId,
+    projectTasksValidator.validateListQuery,
+    projectTasksController.listProjectTasks
+  );
 
 export default router;
