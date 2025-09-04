59f19c449649638c6b9065b39586336e82e997fb
choYoungWook <wwwe2410@naver.com>
2025-09-04 10:46:06 +0900

    refactor/authmiddleware로 수정

--- src/modules/invitations/invitaions.router.ts
+++ src/modules/invitations/invitaions.router.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
-import invitationController from '#modules/invitations/invitations.controller'
-import { requireAuth } from '#middlewares/requireAuth';
+import invitationController from '#modules/invitations/invitations.controller';
+import { authMiddleware } from '#middlewares/authMiddleware';
 
 const router = Router();
 
-router.route('/:invitationId').delete(requireAuth, invitationController.deleteInvitation);
-
-router.route('/:invitationId/accept').post(requireAuth, invitationController.acceptInvitation)
-
+router.route('/:invitationId').delete(authMiddleware, invitationController.deleteInvitation);
+router.route('/:invitationId/accept').post(authMiddleware, invitationController.acceptInvitation);
 
 export default router;
